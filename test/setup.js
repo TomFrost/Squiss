be9165543d48c3b10551edf5fa4cb4f6d0189e43
TomFrost <tom@frosteddesign.com>
2015-11-23 17:23:46 -0500

    NPM: Remove unused dev deps

--- test/setup.js
+++ test/setup.js
@@ -3,12 +3,10 @@
  */
 
 import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import path from 'path';
 import module from 'module';
 import sinon from 'sinon';
 
-chai.use(chaiAsPromised);
 global.should = chai.should();
 global.sinon = sinon;
 
